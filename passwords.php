f848190bac13f556b5627325ab600b3374bc39f6
Mongh <Mongh1993@gmail.com>
2019-09-21 08:57:44 +0700

    passwords.php
    laravel-5-Laos-lang

---
+++ passwords.php
@@ -0,0 +1,19 @@
+  
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'ລະຫັດຜ່ານຕ້ອງມີຢ່າງຫນ້ອຍ 6 ຕົວອັກສອນແລະກົງກັບການຢືນຢັນ.',
+    'reset'    => 'ລະຫັດຜ່ານຂອງທ່ານໄດ້ຖືກຕັ້ງຄ່າໃໝ່ ແລ້ວ!',
+    'sent'     => 'ພວກເຮົາໄດ້ສົ່ງລິ້ງການຕັ້ງຄ່າລະຫັດຜ່ານໃຫ້ທ່ານຜ່ານທາງອີເມວ!',
+    'token'    => 'ການຕັ້ງຄ່າລະຫັດຜ່ານນີ້ແມ່ນບໍ່ຖືກຕ້ອງ.',
+    'user'     => 'ພວກເຮົາບໍ່ສາມາດຊອກຫາຜູ້ໃຊ້ທີ່ມີທີ່ຢູ່ອີເມວນັ້ນ.',
+];
